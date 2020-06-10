31b34538bbfac38915b1d77fbfe3b864174f14a2
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-10 16:00:09 -0700

    delete no-need files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import React from 'react';
-import './styles/index.css';
-import HamburgerIcon from './component/util/HamburgerIcon'
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css'
+import AboutMe from './component/AboutMe/AboutMe';
+import HamburgerIcon from './component/BurgerIcon/HamburgerIcon'
+import { ContactMe } from './component/ContactMe/ContactMe';
+import CurriculumVitae from './component/CurriculumVitae/CurriculumVitae';
+import HomeButton from './images/home.svg';
 
-function App() {
-  return (
-    <div className="App">    
-      
-    <header>
-        <div className="HamburgerIcon">
-          <HamburgerIcon /> 
-        </div>      
-    </header>         
-    
-      <div>       
-        <h1>Hi I am Daminda Dinesh</h1>
-        <h2>Your Full-Stack Developer</h2>
-      </div>
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.showMainMenuOptions = this.showMainMenuOptions.bind(this);
+    this.state = {};  
+  }
+  
+  componentDidMount(){
+    this.showMainMenuOptions();  
+  }
+
+  showMainMenuOptions(option){
+     switch(option){       
+       case "aboutme" :
+          this.setState( { currentScreen : <AboutMe /> }) ;
+          break;     
+      case "contactme":
+        this.setState({ currentScreen : <ContactMe />});
+        break;
+      case "CV":
+        this.setState({ currentScreen: <CurriculumVitae />});
+        break;
+      default: this.setState( { currentScreen: (<div className="col intro-text-container">   
+      <span className="my-name">Hi, I am Daminda Dinesh</span>
+      <span className="my-intro">Your Full-Stack Developer</span>
+      </div>) });
+        
+     }
+  }
+
+  render(){   
+    return(
+    <div className="container App">        
+        <div className="row">
+              <div className="col">
+                  <img onClick={ ()=>this.showMainMenuOptions()}
+                  className="home-button" src={  HomeButton } alt="Home" />
+              </div>
+              <div className="col HamburgerIcon">      
+                               
+                        <HamburgerIcon showMainMenuOptions = { this.showMainMenuOptions }/>                
+              </div>   
+                    
+                {   this.state.currentScreen } 
+                      
+        </div>         
+        <div className="col go-down-arrow">
+                <div><p>See my work</p>
+                 <div className="go-down-arrow-1"></div>
+                 <div className="go-down-arrow-2"></div>
+                 </div>
+          </div>
     </div>
-  );
+    );
+  }
+
+
+
 }
+
 
 export default App;
