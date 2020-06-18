de81b382b6d639528ae58741b0f627e465abd78a
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-17 18:23:09 -0700

    add production code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
@@ -11,7 +12,12 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users",users);
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 mongoose
     .connect( db, { useNewUrlParser : true })
