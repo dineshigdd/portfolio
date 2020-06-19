7e8b9e48c5540ec6e093699e39e265ff722fdec3
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-18 19:47:33 -0700

    channg in server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ mongoose
     .catch( err => console.log( err ))
     
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-     // app.use(express.static(__dirname,'../frontend/build'));
+    //app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+     app.use(express.static(__dirname,'../frontend/build/static'));
       app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
       })
