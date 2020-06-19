89d810ef535c0d2a34539f75ac3e978b3c2c7f5f
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-18 19:51:03 -0700

    add build folder

---
+++ frontend/build/precache-manifest.5aa8c675c1ff4e54ea826bc32b578c40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947967ce0e84803260c204ae624569be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7b840310f77c29a51ff",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "3d60100cbd995521c750",
+    "url": "/static/css/main.6d309fb2.chunk.css"
+  },
+  {
+    "revision": "d7b840310f77c29a51ff",
+    "url": "/static/js/2.197675f9.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.197675f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d60100cbd995521c750",
+    "url": "/static/js/main.a7796a3f.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "7b2324ae8187904220236d76d323d474",
+    "url": "/static/media/home.7b2324ae.svg"
+  },
+  {
+    "revision": "2b2c61599db7b2eabb413e0299a23944",
+    "url": "/static/media/self.779110ac.jpg"
+  }
+]);
