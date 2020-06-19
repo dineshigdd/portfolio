5fd07695f4ef27531e3a7ea1ade74aec324bfc2e
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-18 20:41:23 -0700

    remove build

--- backend/server.js
+++ backend/server.js
@@ -21,18 +21,12 @@ mongoose
     
 if (process.env.NODE_ENV === 'production') {
     //app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-     app.use(express.static(__dirname,'../frontend/build/'));
-     router.get('/', function (req, res) {
-      res.sendFile(path.join(__dirname, '../../frontend/index.html'));
-    });
-      // app.get('/', (req, res) => {
-      //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-      // })
+     app.use(express.static(__dirname,'../frontend/build/static'));
+      app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      })
     }
 
 const port = process.env.PORT || 5000;
 app.listen( port, () => console.log( `Server is running on port ${port}`))
 
-router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../client/index.html'));
-});
