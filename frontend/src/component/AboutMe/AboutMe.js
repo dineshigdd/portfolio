a014656fca5e4968274a25568faf1eaaa62da212
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-19 10:26:50 -0700

    comment all frontend component

--- frontend/src/component/AboutMe/AboutMe.js
+++ frontend/src/component/AboutMe/AboutMe.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import '../AboutMe/AboutMe.css'
-import myImage from "../../images/self.jpg";
+// import React from 'react';
+// import '../AboutMe/AboutMe.css'
+// import myImage from "../../images/self.jpg";
 
-class AboutMe extends React.Component{
+// class AboutMe extends React.Component{
     
     
-    render(){        
-        return(
-            <div className="container">                
-                <div className="row about-me-row">
-                    <div className="col s12 myImage-wrapper">
-                        <img className="myImage" src={ myImage } alt="myImage"/>
-                    </div>               
+//     render(){        
+//         return(
+//             <div className="container">                
+//                 <div className="row about-me-row">
+//                     <div className="col s12 myImage-wrapper">
+//                         <img className="myImage" src={ myImage } alt="myImage"/>
+//                     </div>               
               
-                    <div className="col s12 my-description-wrapper">
-                        <div>
-                            <p>My name is Dinesh. I am enthiasitice about web developement and programming
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </div>
-                    </div>                   
-                    <div className="col s12 social-media-icons">
-                        <a href="https://www.facebook.com/daminda.me/"><i className="fab fa-facebook fa-2x fa-fw" aria-hidden="true"></i></a>
-                        {/* <a href="#"><i className="fab fa-twitter fa-2x  fa-fw" aria-hidden="true"></i></a>
-                        <a href="#"><i className="fab fa-linkedin fa-2x fa-fw" aria-hidden="true"></i></a>
-                        <a href="#"><i className="fab fa-github-square fa-2x fa-fw" aria-hidden="true"></i></a> */}
-                    </div>
+//                     <div className="col s12 my-description-wrapper">
+//                         <div>
+//                             <p>My name is Dinesh. I am enthiasitice about web developement and programming
+//                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+//                             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+//                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+//                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+//                              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+//                             </p>
+//                         </div>
+//                     </div>                   
+//                     <div className="col s12 social-media-icons">
+//                         <a href="https://www.facebook.com/daminda.me/"><i className="fab fa-facebook fa-2x fa-fw" aria-hidden="true"></i></a>
+//                         {/* <a href="#"><i className="fab fa-twitter fa-2x  fa-fw" aria-hidden="true"></i></a>
+//                         <a href="#"><i className="fab fa-linkedin fa-2x fa-fw" aria-hidden="true"></i></a>
+//                         <a href="#"><i className="fab fa-github-square fa-2x fa-fw" aria-hidden="true"></i></a> */}
+//                     </div>
     
-                </div>
-            </div>
+//                 </div>
+//             </div>
 
-        );
-    }
-}
+//         );
+//     }
+// }
 
-export default AboutMe;
+// export default AboutMe;
