8097332695eee4add0c81546bcfaabf67c0d7c84
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-19 10:41:05 -0700

    comment all frontend component

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
 
-const UserSchema  = new Schema({
-    firstName : {
-        type: String,
-        required: true
-    },
+// const UserSchema  = new Schema({
+//     firstName : {
+//         type: String,
+//         required: true
+//     },
 
-    lastName : {
-        type: String,
-        required: true
-    },
+//     lastName : {
+//         type: String,
+//         required: true
+//     },
 
-    email :{
-        type: String,
-        required: true
-    },
+//     email :{
+//         type: String,
+//         required: true
+//     },
 
-    phone : {
-        type: String,
-        required: false
-    }
-});
+//     phone : {
+//         type: String,
+//         required: false
+//     }
+// });
 
-module.exports = user = mongoose.model('User', UserSchema);
+// module.exports = user = mongoose.model('User', UserSchema);
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../../models/user')
-const Message = require('../../models/message');
+// const express = require('express');
+// const router = express.Router();
+// const User = require('../../models/user')
+// const Message = require('../../models/message');
 
 
 
-router.get("/test", (req, res) => res.json({msg : "this is users"}));
+// router.get("/test", (req, res) => res.json({msg : "this is users"}));
 
-router.post("/sendmsg",(req, res) => {
-   const CONFIRMATION = "Your message has been submited. Thank you"
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (user) {
-            new Message({
-            message : req.body.message,
-            user: user._id
-          }).save().then(() => res.json({ msgConfirm : CONFIRMATION }))
-        } else {
-          // Otherwise create a new user
-          const newUser = new User({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    email: req.body.email,
-                    phone: req.body.phone
-                });
+// router.post("/sendmsg",(req, res) => {
+//    const CONFIRMATION = "Your message has been submited. Thank you"
+//     User.findOne({ email: req.body.email })
+//       .then(user => {
+//         if (user) {
+//             new Message({
+//             message : req.body.message,
+//             user: user._id
+//           }).save().then(() => res.json({ msgConfirm : CONFIRMATION }))
+//         } else {
+//           // Otherwise create a new user
+//           const newUser = new User({
+//                     firstName: req.body.firstName,
+//                     lastName: req.body.lastName,
+//                     email: req.body.email,
+//                     phone: req.body.phone
+//                 });
                 
-                newUser.save();
+//                 newUser.save();
 
-                const message = new Message({
-                    message : req.body.message,
-                    user: newUser._id
-                });
+//                 const message = new Message({
+//                     message : req.body.message,
+//                     user: newUser._id
+//                 });
 
-                message.save().then(() => res.json({ msgConfirm : CONFIRMATION }))
+//                 message.save().then(() => res.json({ msgConfirm : CONFIRMATION }))
   
-                // res.json({ user: newUser, msg: message })
-        }
-      })
-  })
+              
+//         }
+//       })
+//   })
 
 
 
-module.exports = router;
+// module.exports = router;
