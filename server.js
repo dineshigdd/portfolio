0d224c81e49c2ff1febd3e909cffaf68079e377d
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-20 10:32:32 -0700

    change

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
       app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
       })
-    }
+    } 
 
 const port = process.env.PORT || 5000;
 app.listen( port, () => console.log( `Server is running on port ${port}`))
