5f33eb82f5d1244d888f609fd95ed86426f5b3be
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-21 12:19:01 -0700

    change

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const users = require('./routes/api/users')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.get("/", (req, res) => res.send("Hello dinesh"));
 app.use("/api/users",users);
 
 
@@ -18,12 +17,12 @@ mongoose
     .then( () => console.log("Connected to MongoDB successfully"))
     .catch( err => console.log( err ))
     
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
       app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
       })
-    // } 
+    } 
 
 const port = process.env.PORT || 5000;
 app.listen( port, () => console.log( `Server is running on port ${port}`))
