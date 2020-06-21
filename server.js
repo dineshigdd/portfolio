ab7d9ae69496f464df8b767a15b6c7898c6882df
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-06-21 11:36:23 -0700

    change server

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ mongoose
     .then( () => console.log("Connected to MongoDB successfully"))
     .catch( err => console.log( err ))
     
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
       app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
       })
-    } 
+    // } 
 
 const port = process.env.PORT || 5000;
 app.listen( port, () => console.log( `Server is running on port ${port}`))
