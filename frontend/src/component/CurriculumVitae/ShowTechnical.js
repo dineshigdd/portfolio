3edc7b0319d9f833173b999fc2e545ce132f961d
dineshigdd@gmail.com <dineshigdd@gmail.com>
2021-02-05 11:11:49 -0800

    modify technical

--- frontend/src/component/CurriculumVitae/ShowTechnical.js
+++ frontend/src/component/CurriculumVitae/ShowTechnical.js
@@ -10,20 +10,32 @@ const ShowTechnical = () =>{
       <div className="cv-tec-link-move">
           <p>Technical</p>
       </div>
-    <div className="showTechnical-wrapper cv-main-display-move">
+      <div className="showTechnical-wrapper cv-main-display-move">
           <div className="level-title">
               <span>beginner</span>
               <span>intermediate</span>
               <span>Advanced</span>
           </div>
           { BioData.technical.language.map( (element, idx) => 
-          <li className="language-item" key={idx}>{ element.name }  <div className="levelIndicator-Hzline">
-            <div className="levelIndicator-Vline"></div></div> </li>
+          <li className="language-item" key={idx}>{ element.name }  
+            <div className="levelIndicator-Hzline">
+            <div className="levelIndicator-Vline"></div>
+            </div>
+          </li>
           
           
           ) }
           
-          </div>
+      </div>
+      <div className="framwork-item-wrapper cv-main-display-move">
+            { BioData.technical.Framework.map( (element, idx) =>(               
+                <li className="framwork-item" key={idx}>
+                  { element}
+                </li>  
+                )           
+
+            )}       
+      </div>   
     </div>
   );
 }
