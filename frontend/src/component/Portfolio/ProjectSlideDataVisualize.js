202c29740ab7f4aa0af4f7d6b7db1bebe376bd00
dineshigdd@gmail.com <dineshigdd@gmail.com>
2021-03-21 09:40:57 -0700

    add projectslideDataVisualize.js

---
+++ frontend/src/component/Portfolio/ProjectSlideDataVisualize.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import './ProjectSlide.css';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import { Divider } from '@material-ui/core';
+
+
+function ProjectSlide({ dataVisualImages, displayProject }) {
+    const [index, setIndex] = useState(0);   
+
+    let images = Object.values(dataVisualImages);  
+    let keys = Object.keys(dataVisualImages);
+
+  
+    const nextImage = () =>{      
+        if( index >= 0 && index <= 2 ){
+            setIndex( index + 1 );
+        }
+    }
+   
+    const previousImage = () =>{        
+        if( index > 0 ){
+            setIndex( index - 1 );           
+        }
+    }
+   
+
+    return (    
+        <div className="slide-wrapper">
+         {/* <em className="left" onClick= { nextProject() }></em> */}
+       
+            <ArrowLeftIcon 
+                style={{ fontSize: '6rem', marginTop:'60px' } }
+                onClick={ ()=> nextImage() }/>
+        
+        <div className="project-slide-container">        
+           
+            <li className="project-slide slide-one" onClick={ ()=>displayProject(keys[ 0 + index ]) }>
+                <img src={ images[ 0 + index ] } alt="barchart"/>
+            </li>
+            <li className="project-slide" onClick={ ()=>displayProject(keys[ 1 + index ])}>
+                <img src={ images[1 + index ] } alt="scstterplot"/>
+            </li>
+            <li className="project-slide" onClick={ ()=>displayProject(keys[ 2 + index ])}>
+                <img src={ images[2 + index ] } alt="heatmap"/>
+            </li>
+            <li className="project-slide" onClick={ ()=>displayProject(keys[ 3 + index ])}>
+                <img src={ images[3 + index ] } alt="chloromap"/>    
+            </li>  
+            
+        </div> 
+       
+            <ArrowRightIcon 
+                style={{ fontSize: '6rem', marginTop:'60px' } }
+                onClick = { ()=>previousImage() }/>
+        
+        
+        {/* <em className="right"></em> */}
+        </div>
+    )
+}
+
+export default ProjectSlide
